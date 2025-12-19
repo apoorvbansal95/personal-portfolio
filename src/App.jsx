ab966775a71bff78421b519526f3ab8fa22f01f3
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-12-19 16:41:27 -0500

    education fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import './App.css'
 import Experience from './components/Experience'
-import Education from './components/education'
+import Education from './components/Education'
 import emailjs from "@emailjs/browser"
 import { motion } from 'framer-motion'
 
